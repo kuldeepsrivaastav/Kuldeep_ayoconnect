bf95174d61218f279f27abbd2a098185b6dee99c
kuldeep <kuldeepsrivastav95@gmail.com>
2021-02-24 01:43:42 +0530

    complete assignment

--- main.go
+++ main.go
@@ -1,11 +1,36 @@
 package main
 
 import (
+	Binary_Tree_Assignment "Kuldeep-GoLangCodingExcersie-GolangSostwareEngineer/Binary-Tree-Assignment"
 	House_Robber "Kuldeep-GoLangCodingExcersie-GolangSostwareEngineer/House-Robber"
 	"fmt"
 )
 
+
 func main()  {
+
+	fmt.Println("Binary Search Tree")
+	var t Binary_Tree_Assignment.Tree
+
+	t.Insert(10)
+	t.Insert(20)
+	t.Insert(40)
+	t.Insert(60)
+	t.Insert(30)
+	t.Insert(50)
+
+	fmt.Printf("In Order: ")
+	Binary_Tree_Assignment.PrintInOrder(t.Root)
+	fmt.Println()
+	fmt.Printf("Pre Order: ")
+	Binary_Tree_Assignment.PrintPreOrder(t.Root)
+	fmt.Println()
+	fmt.Printf("Post Order: ")
+	Binary_Tree_Assignment.PrintPostOrder(t.Root)
+	fmt.Println()
+
+	fmt.Println("---------------------------------------------")
+	fmt.Println("House Robber")
 	slc:=[]int{1,2,3,1}
-	fmt.Print(House_Robber.Rob(slc))
+	fmt.Println(House_Robber.Rob(slc))
 }
